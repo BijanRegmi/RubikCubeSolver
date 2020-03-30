a1e0e920272fc48c923964efb91c231d70b140eb
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2020-03-30 19:02:03 +0000

    DEFINED MOVES

---
+++ cube.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+test = [
+    ["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"]
+]
+
+white = [
+    ["w","w","w"],
+    ["w","w","w"],
+    ["w","w","w"]
+]
+white = np.array(white)
+
+yellow = [
+    ["y","y","y"],
+    ["y","y","y"],
+    ["y","y","y"]
+]
+yellow = np.array(yellow)
+
+green = [
+    ["g","g","g"],
+    ["g","g","g"],
+    ["g","g","g"]
+]
+green = np.array(green)
+
+blue = [
+    ["b","b","b"],
+    ["b","b","b"],
+    ["b","b","b"]
+]
+blue = np.array(blue)
+
+red = [
+    ["r","r","r"],
+    ["r","r","r"],
+    ["r","r","r"]
+]
+red = np.array(red)
+
+orange = [
+    ["o","o","o"],
+    ["o","o","o"],
+    ["o","o","o"]
+]
+orange = np.array(orange)
