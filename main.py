40951dcd6221761898bb9a4e8df61c4a38b88ea2
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2020-04-01 23:48:01 +0545

    Added function to convert current cube state to kociemba format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import moves
 import cube
-
+import kociemba
 cols = ["green","blue","white","yellow","red","orange"]
 
 def show(side):
@@ -37,24 +37,58 @@ def show(side):
         buts[i].grid(row=int(n/3)+6,column=int(n%3)+4)    #starts from row = 6 and column = 4
         n+=1
 
-    #surroundings
     Button(root,width=3,bg=cols[int(surr[0])],state=DISABLED).grid(row=4,column=5)
     Button(root,width=3,bg=cols[int(surr[1])],state=DISABLED).grid(row=7,column=8)
     Button(root,width=3,bg=cols[int(surr[2])],state=DISABLED).grid(row=10,column=5)
     Button(root,width=3,bg=cols[int(surr[3])],state=DISABLED).grid(row=7,column=2)
-
     Button(root,width=6,text="EDIT").grid(row=1,column=1,columnspan=2,rowspan=2)
     Button(root,width=6,text="SOLVE").grid(row=1,column=8,columnspan=2,rowspan=2)
-
     Label(root).grid(row=0,column=0,columnspan=11)
     Label(root).grid(row=3,column=0,columnspan=11)
     Label(root).grid(row=5,column=0,columnspan=11)
     Label(root).grid(row=9,column=0,columnspan=11)
     Label(root).grid(row=11,column=0,columnspan=11)
     Label(root).grid(row=13,column=0,columnspan=11)
-    
+    Button(root,width=3,bg=cols[0]).grid(row=1,column=4)
+    Button(root,width=3,bg=cols[1]).grid(row=2,column=4)
+    Button(root,width=3,bg=cols[2]).grid(row=1,column=5)
+    Button(root,width=3,bg=cols[3]).grid(row=2,column=5)
+    Button(root,width=3,bg=cols[4]).grid(row=1,column=6)
+    Button(root,width=3,bg=cols[5]).grid(row=2,column=6)
     Entry(root,width=40).grid(row=12,column=0,columnspan=11)
     
+def current_state():
+    state = ""
+    for i in cube.white:
+        for j in i:
+            state+=selecttext(j)
+    for i in cube.red:
+        for j in i:
+            state+=selecttext(j)
+    for i in cube.green:
+        for j in i:
+            state+=selecttext(j)
+    for i in cube.yellow:
+        for j in i:
+            state+=selecttext(j)
+    for i in cube.orange:
+        for j in i:
+            state+=selecttext(j)
+    for i in cube.blue:
+        for j in i:
+            state+=selecttext(j)
+    return state
+            
+def selecttext(c):
+    f = {
+        "w":"U",
+        "r":"R",
+        "y":"D",
+        "o":"L",
+        "g":"F",
+        "b":"B",
+    }.get(c)
+    return f
 
 root = Tk()
 root.title("RUBIK CUBE SOLVER")
@@ -70,15 +104,5 @@ buts = [
         Button(root,width=3)
     ]
 
-Button(root,width=3,bg=cols[0]).grid(row=1,column=4)
-Button(root,width=3,bg=cols[1]).grid(row=2,column=4)
-Button(root,width=3,bg=cols[2]).grid(row=1,column=5)
-Button(root,width=3,bg=cols[3]).grid(row=2,column=5)
-Button(root,width=3,bg=cols[4]).grid(row=1,column=6)
-Button(root,width=3,bg=cols[5]).grid(row=2,column=6)
-
-labs = [1,2,3,4,5]
-#for i in range(5):
-#    labs[i] = Label(root,text="").grid(row=i,column=0)
 show(1)
 root.mainloop()
